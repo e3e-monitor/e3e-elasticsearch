7cebdcf8b532d76f900931dcc5401c94823950c9
Brice Copy <brice.copy@gmail.com>
2015-10-08 09:11:07 +0200

    Tweet template

--- logstash/upload_templates.sh
+++ logstash/upload_templates.sh
@@ -2,3 +2,4 @@ curl -XPUT $E3E_HOSTNAME/idp/_mapping/idpcamp --data-binary @data/input/idp/temp
 curl -XPUT $E3E_HOSTNAME/refugee/_mapping/refugeecamp --data-binary @data/input/refugee/template.json
 curl -XPUT $E3E_HOSTNAME/geonames/_mapping/geoname --data-binary @data/input/geonames/template.json
 curl -XPUT $E3E_HOSTNAME/geojson/_mapping/feature --data-binary @data/input/geojson/template.json
+curl -XPUT $E3E_HOSTNAME/tweets/_mapping/tweet --data-binary @data/input/tweets/template.json
